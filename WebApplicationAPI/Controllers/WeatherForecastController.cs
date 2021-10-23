927c905b34a6b353faaf16f5ef2e40159ec6e6d8
Kurt <kdeneve@gmail.com>
2021-10-24 01:07:19 +0200

    add kurt to list

--- WebApplicationAPI/Controllers/WeatherForecastController.cs
+++ WebApplicationAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebApplicationAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Kurt", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
